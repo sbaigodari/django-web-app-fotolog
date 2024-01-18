4e4126922bf9ab1c62b59d72ab373673331693f1
Shalom Love <shalombaigodari@gmail.com>
2024-01-18 17:59:13 +0100

    Ajout multiple televervesement et restriction

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,43 @@
 # blog/models.py
 from django.conf import settings
 from django.db import models
-
+from PIL import Image
 
 class Photo(models.Model):
+
     image = models.ImageField()
     caption = models.CharField(max_length=128, blank=True)
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     
+    IMAGE_MAX_SIZE = (800,800)
+
+    def resize_image(self):
+
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+
+        super().save(*args,**kwargs)
+        self.resize_image()
 
 class Blog(models.Model):
+
     photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
     title = models.CharField(max_length=128)
     content = models.CharField(max_length=5000)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     starred = models.BooleanField(default=False)
+    word_count = models.IntegerField(null=True)
+
+    def _get_word_count(self):
+
+        return len(self.content.split(' '))
+
+    def save(self,*args,**kwargs):
+        self.word_count = self._get_word_count()
+        super().save(*args,**kwargs)
+        
--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -35,6 +35,8 @@ urlpatterns = [
     path('profile-photo/upload', authentification.views.upload_profil_photo, name='upload_profil_photo'),
     path('blog/create/',blog.views.blog_and_photo_upload,name='blog_create'),
     path('blog/<int:blog_id>', blog.views.view_blog, name='view_blog'),
+    path('blog/<int:blog_id>/edit', blog.views.edit_blog, name='edit_blog'),
+    path('photo/upload-multiple/', blog.views.create_multiple_photos,name='create_multiple_photos'),
 ]
 
 if settings.DEBUG :

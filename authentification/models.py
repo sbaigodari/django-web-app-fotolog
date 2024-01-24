265607f5e60377c10f7290cccfabdcde5afe68c9
Shalom Love <shalombaigodari@gmail.com>
2024-01-24 11:17:44 +0100

    fix migrations bug

--- authentification/models.py
+++ authentification/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,Group
 from django.db import models
 
 class User(AbstractUser):
@@ -12,5 +12,23 @@ class User(AbstractUser):
     SUBSCRIBER= 'SUBSCRIBER'
     profile_photo = models.ImageField(verbose_name='Photo de profil')
     role = models.CharField(max_length=30,choices=ROLE_CHOICES.choices,verbose_name='Rôle')
+    follows = models.ManyToManyField(
+        'self',
+        limit_choices_to={'role': CREATOR},
+        symmetrical=False,
+        verbose_name='suit',
+    )
+
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+
+        if self.role == self.CREATOR:
+            
+            group = Group.objects.get(name='creators')
+            group.user_set.add(self)
+
+        elif self.role == self.SUBSCRIBER:
+            group = Group.objects.get(name='subscribers')
+            group.user_set.add(self)
 
 
